e16ab62c7756180968d60e39fc9d84acf9c57c3e
hiranyapiumath@gmail.com <hiranyapiumath@gmail.com>
2026-04-15 20:08:17 +0530

    feat: add AppDbContext, EF Core migrations, seed research areas

--- BlindMatchPAS.Web/Program.cs
+++ BlindMatchPAS.Web/Program.cs
@@ -1,6 +1,11 @@
+using BlindMatchPAS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
